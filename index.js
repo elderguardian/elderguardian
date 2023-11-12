57840105e8e895efd044a39068e9739d91a8731b
elderguardian <katzenmaedchen@tutanota.com>
2023-11-12 14:07:52 +0100

    update readme

--- index.js
+++ index.js
@@ -20,16 +20,19 @@ const unsplash = createApi({
   const authorName = photo.response.user.username;
   const authorHtmlUrl = `https://unsplash.com/@${authorName}?utm_source=${appName}&amp;utm_medium=referral`;
 
-  const readmeContent = '| <div align="left">'
-    + '<h3>Hey, I am Luca</h3> <p>A student and hobby developer in love with software.</p>'
-    + ' <pre>Based in Hessen, Germany<br>Fachoberschule Student<br>I mostly use JavaScript/TypeScript, PHP and Java<br>Mostly Self-taught using the Internet</pre>'
+  const readmeContent = '<div align="center">\n\n | <div align="left">'
+    + '<h3>👋 Hey, I am Luca</h3> <p>A student and hobby developer in love with software.</p>'
+    + ' <pre>🇩🇪 Based in Hessen, Germany<br>🧑‍🎓 Fachoberschule Student<br>👨‍💻 I mostly use JavaScript/TypeScript, PHP and Java<br>🏫 Mostly Self-taught using the Internet</pre>'
+    + '<p>Learn more about me:</p>'
+    + '<ul><li><a href="https://kagerou.dev/" target="_blank">Kagerou</a> - My landing page</li>'
+    + '<li><a href="https://elderguardian.github.io/blog/" target="_blank">The Ocean Monument</a> - My articles</li></ul>'
     + '</div> | <div align="center">'
-    + '<h3>Today\'s random image:</h3>'
-    + `<img height="120" src="${imageUrl}" alt="Random Image from Unsplash API."><br>`
-    + `Photo by <a href="${authorHtmlUrl}" target="_blank_" rel="noreferrer noopener">${authorName}</a> `
+    + '<h3>🎲 Today\'s random image:</h3>'
+    + `<img height="200" src="${imageUrl}" alt="Random Image from Unsplash API."><br><br>`
+    + `📷 Photo by <a href="${authorHtmlUrl}" target="_blank_" rel="noreferrer noopener">${authorName}</a> `
     + `on <a href="${imageHtmlUrl}" target="_blank" rel="noreferrer noopene">Unsplash</a>`
     + '</div><br> |\n'
-    + '|---|---|'
+    + '|---|---|\n\n</div>'
 
   console.log(readmeContent);
 })();
