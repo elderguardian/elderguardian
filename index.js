0f86f2ac209f6df8ecf7e065728adca205a07e88
elderguardian <katzenmaedchen@tutanota.com>
2023-11-12 01:04:58 +0100

    updated profile readme layout

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const unsplash = createApi({
 });
 
 (async () => {
-  const photo = await unsplash.photos.getRandom({});
+  const photo = await unsplash.photos.getRandom({
+    orientation: "landscape",
+  });
 
   if (photo.type !== "success") {
     throw new Error("Could not get a random Photo.");
@@ -18,13 +20,16 @@ const unsplash = createApi({
   const authorName = photo.response.user.username;
   const authorHtmlUrl = `https://unsplash.com/@${authorName}?utm_source=${appName}&amp;utm_medium=referral`;
 
-  const readmeContent =
-    "`Today's random Image:`\n" +
-    "<div>\n<br>\n" +
-    `<img height="300" src="${imageUrl}" alt="Random Image from Unsplash API.">\n` +
-    "<br>\n<br>\n</div>\n\n" +
-    `Photo by <a href="${authorHtmlUrl}" target="_blank_" rel="noreferrer noopener">${authorName}</a> ` +
-    `on <a href="${imageHtmlUrl}" target="_blank" rel="noreferrer noopene">Unsplash</a>`;
+  const readmeContent = '| <div align="left">'
+    + '<h3>Hey, I am Luca</h3> <p>A student and hobby developer in love with software.</p>'
+    + ' <pre>Based in Hessen, Germany<br>Fachoberschule Student<br>I mostly use JavaScript/TypeScript, PHP and Java<br>Mostly Self-taught using the Internet</pre>'
+    + '</div> | <div align="center">'
+    + '<h3>Today\'s random image:</h3>'
+    + `<img height="120" src="${imageUrl}" alt="Random Image from Unsplash API."><br>`
+    + `Photo by <a href="${authorHtmlUrl}" target="_blank_" rel="noreferrer noopener">${authorName}</a> `
+    + `on <a href="${imageHtmlUrl}" target="_blank" rel="noreferrer noopene">Unsplash</a>`
+    + '</div><br> |\n'
+    + '|---|---|'
 
   console.log(readmeContent);
 })();
